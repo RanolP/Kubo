4c317f033a846fc8376b8accc237d521f6f22f82
Ranol <asdhugh1@gmail.com>
2017-09-08 21:03:08 +0900

    Update Option

--- Kubo-api/src/main/kotlin/io/github/ranolp/kubo/general/Option.kt
+++ Kubo-api/src/main/kotlin/io/github/ranolp/kubo/general/Option.kt
@@ -1,5 +1,6 @@
 package io.github.ranolp.kubo.general
 
 import io.github.ranolp.kubo.general.side.Side
+import io.github.ranolp.kubo.general.side.Sided
 
-open class Option(val side: Side) {}
+open class Option(override val side: Side) : Sided {}
