9100954abb625edc4dd86cb767847cb78f714627
Ranol <asdhugh1@gmail.com>
2017-09-10 13:31:47 +0900

    Surround with try ~ catch

--- Kubo-api/src/main/kotlin/io/github/ranolp/kubo/Kubo.kt
+++ Kubo-api/src/main/kotlin/io/github/ranolp/kubo/Kubo.kt
@@ -4,20 +4,27 @@ import io.github.ranolp.kubo.general.Option
 import io.github.ranolp.kubo.general.objects.User
 import io.github.ranolp.kubo.general.side.Side
 
-fun setUp(adapter1: KuboAdapter<Option>, vararg adapters: KuboAdapter<Option>) {
+fun setUp(adapter1: KuboAdapter<Option>, vararg adapters: KuboAdapter<Option>, ignoreError: Boolean = true) {
     info("Setup adapters...")
     info("Inserted adapter count : ${adapters.size + 1}")
     setUp(adapter1)
     for (adapter in adapters) {
-        setUp(adapter)
+        setUp(adapter, ignoreError)
     }
 }
 
-fun <T : KuboAdapter<Option>> setUp(adapter: T) {
+fun <T : KuboAdapter<Option>> setUp(adapter: T, ignoreError: Boolean = true) {
     info("Setup ${adapter::class.simpleName}...")
     Adapters.put(adapter.side, adapter)
-    adapter.login()
-    info("Login success.")
+    try {
+        adapter.login()
+        info("Login success.")
+    } catch(e: Throwable) {
+        logError(e)
+        if (!ignoreError) {
+            System.exit(0)
+        }
+    }
 }
 
 fun start(bot: Bot) {
